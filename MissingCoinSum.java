990f7e7d1216d5e2d9700435aec5acc19d47bff1
neelsshah2006 <neelsshah2006@gmail.com>
2026-06-12 15:05:07 +0530

    Missing Coin Sum

---
+++ MissingCoinSum.java
@@ -0,0 +1,61 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+
+public class MissingCoinSum {
+
+    static class FastScanner {
+        private final InputStream in = System.in;
+        private final byte[] buffer = new byte[1 << 16];
+        private int ptr = 0, len = 0;
+
+        private int read() throws IOException {
+            if (ptr >= len) {
+                len = in.read(buffer);
+                ptr = 0;
+                if (len <= 0)
+                    return -1;
+            }
+            return buffer[ptr++];
+        }
+
+        int nextInt() throws IOException {
+            int c;
+            do {
+                c = read();
+            } while (c <= ' ');
+
+            int sign = 1;
+            if (c == '-') {
+                sign = -1;
+                c = read();
+            }
+
+            int val = 0;
+            while (c > ' ') {
+                val = val * 10 + c - '0';
+                c = read();
+            }
+            return val * sign;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        FastScanner fs = new FastScanner();
+        int n = fs.nextInt();
+        int[] coins = new int[n];
+        for (int i = 0; i < n; i++) {
+            coins[i] = fs.nextInt();
+        }
+
+        Arrays.sort(coins);
+        long sum = 0;
+        for(int i = 0; i < n; i++) {
+            if(sum + 1 < coins[i]) {
+                break;
+            }
+            sum += coins[i];
+        }
+        System.out.println(sum + 1);;
+    }
+}
