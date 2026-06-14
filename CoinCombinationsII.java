1516032f35a0f36e8c2d39de7910f935d8ac4fce
neelsshah2006 <neelsshah2006@gmail.com>
2026-06-14 15:34:39 +0530

    .\Coin Combinations II

---
+++ CoinCombinationsII.java
@@ -0,0 +1,62 @@
+import java.io.IOException;
+import java.io.InputStream;
+
+public class CoinCombinationsII {
+    static class FastScanner {
+        private final InputStream in = System.in;
+        private final byte[] buffer = new byte[1 << 16];
+        private int ptr = 0, len = 0;
+
+        private int read() throws IOException {
+            if (ptr >= len) {
+                len = in.read(buffer);
+                ptr = 0;
+                if (len <= 0)
+                    return -1;
+            }
+            return buffer[ptr++];
+        }
+
+        int nextInt() throws IOException {
+            int c;
+            do {
+                c = read();
+            } while (c <= ' ');
+
+            int sign = 1;
+            if (c == '-') {
+                sign = -1;
+                c = read();
+            }
+
+            int val = 0;
+            while (c > ' ') {
+                val = val * 10 + c - '0';
+                c = read();
+            }
+            return val * sign;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        FastScanner fs = new FastScanner();
+        int n = fs.nextInt();
+        int k = fs.nextInt();
+        int[] coins = new int[n];
+        for (int i = 0; i < n; i++) {
+            coins[i] = fs.nextInt();
+        }
+
+        int[] dp = new int[k + 1];
+        dp[0] = 1;
+        int MOD = 1000000007;
+        for (int i = 1; i <= n; i++) {
+            int val = coins[i - 1];
+            for (int j = val; j <= k; j++) {
+                dp[j] = (dp[j - val] + dp[j]) % MOD;
+            }
+        }
+
+        System.out.println(dp[k]);
+    }
+}
