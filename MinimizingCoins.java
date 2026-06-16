8a790396272c57b938b7b1e7ff88835927b1ec9c
neelsshah2006 <neelsshah2006@gmail.com>
2026-06-16 14:46:37 +0530

    Optimized Minimizing Coins

--- MinimizingCoins.java
+++ MinimizingCoins.java
@@ -14,18 +14,16 @@ public class MinimizingCoins {
 
         int[] dp = new int[k + 1];
         Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
         for (int i = 1; i <= n; i++) {
             int val = coins[i - 1];
-            int[] temp = new int[k + 1];
-            temp[0] = 0;
             for (int j = 1; j <= k; j++) {
-                if (j - val >= 0 && temp[j - val] != Integer.MAX_VALUE) {
-                    temp[j] = Math.min(temp[j - val] + 1, dp[j]);
+                if (j - val >= 0 && dp[j - val] != Integer.MAX_VALUE) {
+                    dp[j] = Math.min(dp[j - val] + 1, dp[j]);
                 } else {
-                    temp[j] = dp[j];
+                    dp[j] = dp[j];
                 }
             }
-            dp = temp;
         }
 
         System.out.println(dp[k] == Integer.MAX_VALUE ? -1 : dp[k]);
