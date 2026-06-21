fafad7db32e8784cf1c0a2b3d3c26d237ba399b8
neelsshah2006 <neelsshah2006@gmail.com>
2026-06-21 16:31:14 +0530

    Distinct Values Subarrays II

---
+++ DistinctValuesSubarraysII.java
@@ -0,0 +1,76 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+
+public class DistinctValuesSubarraysII {
+    static class FastScanner {
+        private final InputStream in = System.in;
+        private final byte[] buffer = new byte[1 << 16];
+        private int ptr = 0, len = 0;
+
+        private int read() throws IOException {
+            if (ptr >= len) {
+                len = in.read(buffer);
+                ptr = 0;
+                if (len <= 0)
+                    return -1;
+            }
+            return buffer[ptr++];
+        }
+
+        int nextInt() throws IOException {
+            int c;
+            do {
+                c = read();
+            } while (c <= ' ');
+
+            int sign = 1;
+            if (c == '-') {
+                sign = -1;
+                c = read();
+            }
+
+            int val = 0;
+            while (c > ' ') {
+                val = val * 10 + c - '0';
+                c = read();
+            }
+            return val * sign;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        FastScanner fs = new FastScanner();
+        int n = fs.nextInt();
+        int k = fs.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = fs.nextInt();
+        }
+
+        int start = 0;
+        int end = 0;
+        long countSubarrays = 0;
+        HashMap<Integer, Integer> freq = new HashMap<>();
+        while (start < n && end < n) {
+            freq.put(arr[end], freq.getOrDefault(arr[end], 0) + 1);
+            while (freq.size() == k + 1) {
+                countSubarrays += (end - start);
+                int x = freq.get(arr[start]);
+                if (x == 1)
+                    freq.remove(arr[start]);
+                else
+                    freq.put(arr[start], x - 1);
+                start++;
+            } 
+            end++;
+        }
+
+        while (start < n) {
+            countSubarrays += n - start;
+            start++;
+        }
+
+        System.out.println(countSubarrays);
+    }
+}
